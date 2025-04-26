4ae027349b7eceebcf6ddd84cec53d087510ba16
sudo0015 <sudobash@qq.com>
2025-04-26 09:12:05 +0800

    4.3.2

--- RandomConfig.py
+++ RandomConfig.py
@@ -16,6 +16,6 @@ class Config(QConfig):
 
 
 YEAR = "2025"
-VERSION = "4.3.0"
+VERSION = "4.3.2"
 cfg = Config()
 qconfig.load(os.path.join(os.path.expanduser('~'), '.Random', 'config', 'config.json'), cfg)
--- RandomLauncher.py
+++ RandomLauncher.py
@@ -2,17 +2,55 @@
 
 import sys
 import subprocess
-from qfluentwidgets import Dialog
+import darkdetect
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor, QIcon
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from qfluentwidgets import Dialog, setTheme, Theme, setThemeColor
 
-if str(subprocess.run(['tasklist'], capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW, shell=True)).find("Random.exe") != -1:
-    w = Dialog("Random", "Random已在后台运行。是否重启？")
-    w.setTitleBarVisible(False)
-    w.yesButton.setText("确定")
-    w.cancelButton.setText("取消")
-    if w.exec():
-        subprocess.run("taskkill -f -im Random.exe", shell=True)
-        subprocess.run("RandomMain.exe", shell=True)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Random")
+        self.setWindowIcon(QIcon(":/icon.png"))
+        self.resize(400, 300)
+
+
+class TrayApp:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.main_window = MainWindow()
+        setThemeColor(QColor(9, 81, 41))
+        if str(subprocess.run(['tasklist'], capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW, shell=True)).find("RandomMain.exe") != -1:
+            self.showDialog()
+        else:
+            subprocess.run("RandomMain.exe", shell=True)
+            sys.exit()
+
+    def showDialog(self):
+        w = Dialog("Random", "Random已在后台运行。是否重启？")
+        w.setTitleBarVisible(False)
+        w.yesButton.setText("确定")
+        w.cancelButton.setText("取消")
+        if w.exec():
+            subprocess.run("taskkill -f -im Random.exe", shell=True)
+            subprocess.run("RandomMain.exe", shell=True)
+            sys.exit()
+        else:
+            sys.exit()
+
+    def run(self):
+        sys.exit(self.app.exec())
+
+
+if __name__ == '__main__':
+    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+    if darkdetect.isDark():
+        setTheme(Theme.DARK)
     else:
-        sys.exit()
-else:
-    subprocess.run("RandomMain.exe", shell=True)
+        setTheme(Theme.LIGHT)
+    app = TrayApp()
+    app.run()
